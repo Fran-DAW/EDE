0ed51816a30889dc18a8e6c4d2fee05ec1c9a9be
Fran <fran_te87@hotmail.com>
2020-05-23 22:24:15 +0200

    Segundo cambio del proyecto

--- ede/src/ede/Ede.java
+++ ede/src/ede/Ede.java
@@ -5,7 +5,7 @@ public class Ede {
 	public static void main(String[] args) {
 		
 		System.out.println("Cambios para subir a Github");
-
+		System.out.println("Añadido otro cambio más para subir a Github");
 	}
 
 }
